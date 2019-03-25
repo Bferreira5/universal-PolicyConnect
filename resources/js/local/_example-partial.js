5af1d98b9f1e3043496250becd46c566088bed52
Ben Ferreira <ferreirab@FERREIRAB01-MAC.local>
2019-03-25 17:36:42 -0400

    Cor and Invoice Updates

--- resources/js/local/_example-partial.js
+++ resources/js/local/_example-partial.js
@@ -1,9 +1,27 @@
 
 // add a class of 'intro' to the first paragraph
 // do this on every page apart from the blog page
-if ( !page('blog') )
-{
 
-    document.querySelector('.content > p').classList.add('intro')
-
-}
+window.addEventListener("load", function() {
+	// store tabs variable
+	var myTabs = document.querySelectorAll("ul.nav-tabs > li");
+  function myTabClicks(tabClickEvent) {
+		for (var i = 0; i < myTabs.length; i++) {
+			myTabs[i].classList.remove("active");
+		}
+		var clickedTab = tabClickEvent.currentTarget;
+		clickedTab.classList.add("active");
+		tabClickEvent.preventDefault();
+		var myContentPanes = document.querySelectorAll(".tab-pane");
+		for (i = 0; i < myContentPanes.length; i++) {
+			myContentPanes[i].classList.remove("active");
+		}
+		var anchorReference = tabClickEvent.target;
+		var activePaneId = anchorReference.getAttribute("href");
+		var activePane = document.querySelector(activePaneId);
+		activePane.classList.add("active");
+	}
+	for (i = 0; i < myTabs.length; i++) {
+		myTabs[i].addEventListener("click", myTabClicks)
+	}
+});
